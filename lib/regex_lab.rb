b6133717343caaec410fdb9726972191beb9e834
Stephen Wright <scw248@gmail.com>
2019-01-15 22:04:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[?!.]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  #phone.match(/\d[0-9]{10}|\d[0-9]{10}+-|\d[0-9]{10}+()|\d[0-9]{10}+-()/) ? true : false
+  phone.match(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/)
 end
